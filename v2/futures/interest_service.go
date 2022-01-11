12eb94d02435bc5f4574f106832627cc98eb6833
baixue <baixue@wecash.net>
2022-01-11 15:37:49 +0800

    fix GetOpenInterest json unmarshal error

--- v2/futures/interest_service.go
+++ v2/futures/interest_service.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
-
-	"github.com/adshao/go-binance/v2/common"
 )
 
 type GetOpenInterestService struct {
@@ -27,10 +25,6 @@ func (s *GetOpenInterestService) Do(ctx context.Context, opts ...RequestOption) 
 		r.setParam("symbol", *s.symbol)
 	}
 	data, _, err := s.c.callAPI(ctx, r, opts...)
-	data = common.ToJSONList(data)
-	if err != nil {
-		return res, err
-	}
 	res = new(OpenInterest)
 	err = json.Unmarshal(data, res)
 	if err != nil {
